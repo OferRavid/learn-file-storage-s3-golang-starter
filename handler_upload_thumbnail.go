1871d7f05d8fe482498cdc84ae6cf9776d583ef0
OferRavid <ofer.rvd@gmail.com>
2025-07-03 01:21:46 +0300

    Updated thumbnails cache handling

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -60,7 +60,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-	assetPath := getAssetPath(videoID, mediaType)
+	assetPath := getAssetPath(mediaType)
 	assetDiskPath := cfg.getAssetDiskPath(assetPath)
 
 	thumbnail, err := os.Create(assetDiskPath)
